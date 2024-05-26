50e9d354362c4a93664246dc6504af4d2982a6d0
Alibek Bekzhanov <freed.alibek@gmail.com>
2024-05-26 22:04:06 +0500

    cringe

--- frontend/src/app/pdf/page.tsx
+++ frontend/src/app/pdf/page.tsx
@@ -5,7 +5,6 @@ import { SendIcon, TrashIcon } from "lucide-react";
 import { cookies } from "next/headers";
 import Link from "next/link";
 import { RedirectType, redirect } from "next/navigation";
-import Chat from "./chat";
 
 export default async function page({
 	searchParams,
@@ -22,7 +21,7 @@ export default async function page({
 		};
 	}
 
-	const data = await Axios({
+	await Axios({
 		url: `/check/${id}`,
 		method: "GET",
 	}).catch(() => {
@@ -36,19 +35,13 @@ export default async function page({
 				"use server";
 				let prompt: string | undefined = formData.get("question")?.toString();
 				if (!prompt) return;
-
 				let query: string = "?";
-
 				for (const message of searchParams?.user ?? []) {
-					console.log(message);
-
 					query += `user=${[message]}&`;
 				}
 				for (const message of searchParams?.ai ?? []) {
 					query += `ai=${message}&`;
 				}
-
-				console.log(query);
 
 				await Axios({
 					url: `/prompt/${id}`,
@@ -57,19 +50,46 @@ export default async function page({
 						prompt,
 					},
 				}).then((response) => {
-					response.data = response.data.replace(/\s\t\n\r /g, "_");
-					prompt = prompt!.replace(/\s\t\n\r /g, "_");
+					response.data = response.data.replaceAll(/\s\t\n\r /g, "%20");
+					prompt = prompt!.replaceAll(/\s\t\n\r /g, "%20");
+					console.log(prompt);
 
 					query += `user=${prompt}&ai=${response.data}&`;
 
-					return redirect(`/redirect	${query}`, RedirectType.push);
+					return redirect(`/pdf${query}`, RedirectType.push);
 				});
 			}}
 		>
-			<Chat />
-			<div className={"flex w-full gap-3 fixed bottom-[20px]"}>
-				<div className={"flex gap-2 w-full"}>
+			<div className={"flex flex-1 flex-col-reverse pb-16"}>
+				<div
+					className={
+						"flex flex-col gap-6 justify-between w-full max-h-full overflow-auto"
+					}
+				>
+					{searchParams!.user.map((_, i) => {
+						return (
+							<div key={i} className={"flex flex-col gap-4"}>
+								<div className={"flex flex-col gap-2 self-end"}>
+									<h2 className={"self-end"}>You</h2>
+									<p className={"self-end"}>{searchParams?.user[i]}</p>
+								</div>
+								<div className={"flex flex-col gap-2"}>
+									<h2>AI</h2>
+									<p>{searchParams?.ai[i]}</p>
+								</div>
+							</div>
+						);
+					})}
+				</div>
+			</div>
+			<div
+				className={
+					"flex w-full gap-3 fixed bottom-[20px] left-0 px-4 justify-between"
+				}
+			>
+				<div className={"flex gap-2 w-[90%]"}>
 					<Input
+						className={"w-full"}
 						name={"question"}
 						id={"question"}
 						type={"text"}
@@ -77,7 +97,7 @@ export default async function page({
 						placeholder={"Enter your question"}
 					/>
 					<Button
-						className={"flex gap-1 items-center"}
+						className={"flex gap-2 items-center"}
 						type={"submit"}
 						variant={"outline"}
 					>
@@ -86,16 +106,17 @@ export default async function page({
 					</Button>
 				</div>
 				<Button
-					className={"flex gap-1 items-center"}
+					className={"flex gap-2 items-center"}
 					type={"button"}
 					variant={"secondary"}
 				>
-					<TrashIcon />
 					<Link
+						className={"flex gap-1"}
 						href={{
 							pathname: "/clear",
 						}}
 					>
+						<TrashIcon />
 						Clear data
 					</Link>
 				</Button>
